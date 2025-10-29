a03e15672714415e0271ace466e46c377f774dc6
Marco <gasparmarco92@gmail.com>
2025-10-29 21:07:10 +0000

    Remove unused import of Core from Utils interface

--- agt/client/utils/index.d.ts
+++ agt/client/utils/index.d.ts
@@ -4,11 +4,10 @@ import type { Dialog } from "./dialog";
 import type { Doc } from "./doc";
 import type { Form } from "./form";
 import type { Table } from "./table";
-import { Db } from "@anygridtech/frappe-agt-types/agt/client/utils/db";
-import { Core } from "@anygridtech/frappe-types/client/frappe/core";
+import type { Text } from "./text";
+import type { Db } from "./db";
 
 export interface Utils extends UtilsExtendedFunctions {
-  core: Core;
   brazil: Brazil;
   dialog: Dialog;
   doc: Doc;
