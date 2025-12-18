b216ecdfc4433e585b00830883a872a9bc2726e0
Marco Souza <gasparmarco92@gmail.com>
2025-12-18 10:33:54 -0300

    Refactor InitialAnalysis interface and add ChildTrackerTableDoc
    Signed-off-by: Marco Souza <gasparmarco92@gmail.com>

--- agt/doctype/InitialAnalysis.d.ts
+++ agt/doctype/InitialAnalysis.d.ts
@@ -1,55 +1,58 @@
 import type { FrappeDoc } from "@anygridtech/frappe-types/client/frappe/core";
-import type { CorrectionsChildList } from "./CorrectionsChildList";
-import type { ChecklistTable } from "./ChecklistTable";
+import type { CorrectionsTrackerDoc } from "@anygridtech/frappe-agt-types/agt/doctype/CorrectionsTracker";
+
+export interface ChildTrackerTableDoc extends FrappeDoc {
+    child_tracker_docname?: string;
+    child_tracker_doctype?: string;
+    child_tracker_workflow_state?: string;
+}
 
 export interface InitialAnalysis extends FrappeDoc {
-  client_list: string | undefined;
-  distributor: string | undefined;
-  priority: string | undefined;
-  service_partner_company: string | undefined;
-  ext_fault_code: string | undefined;
-  ext_fault_description: string | undefined;
-  ext_fault_customer_description: string | undefined;
-  ext_fault_support_description: string | undefined;
-  ext_fault_date: string | undefined;
-  ext_fault_video: string | undefined;
-  current_issue_status?: string | undefined;
-  main_eqp_model: string | undefined;
-  main_eqp_family: string | undefined;
-  main_eqp_serial_no: string | undefined;
-  main_eqp_group: string | undefined;
-  main_eqp_warehouse: string | undefined;
-  main_eqp_initial_firmware: string | undefined;
-  main_eqp_final_firmware: string | undefined;
-  main_eqp_has_reseted: number | undefined;
-  main_eqp_is_updated: number | undefined;
-  main_eqp_is_online: number | undefined;
-  main_eqp_has_neutral: number | undefined;
-  main_eqp_has_transformer: number | undefined;
-  main_eqp_has_stringbox: number | undefined;
-  main_eqp_has_smartmeter: number | undefined;
-  main_eqp_has_smartenergy: number | undefined;
-  main_eqp_has_batteries: number | undefined;
-  protocol_type:
-  | "Warranty Inverter"
-  | "Monitoring"
-  | "Ev Charger"
-  | undefined;
-  issue_inverter: string | undefined;
-  issue_monitoring: string | undefined;
-  issue_ev_charger: string | undefined;
-  warranty_claim_inverter: string | undefined;
-  warranty_claim_ev_charger: string | undefined;
-  warranty_claim_battery: string | undefined;
-  corrections_feed?: CorrectionsChildList[];
-  checklist_table_inverter?: ChecklistTable[];
-  checklist_table_ev_charger?: ChecklistTable[];
-  checklist_table_battery?: ChecklistTable[];
-  checklist_table_transformer?: ChecklistTable[];
-  checklist_table_smart_meter?: ChecklistTable[];
-  checklist_table_smart_energy_manager?: ChecklistTable[];
-  inanly_docname?: string
-  ticket_docname?: string
-  // child_table?: string | string[];
-  // child_table_name?: string | string[];
+    // Naming
+    naming_series?: "INANLY-.MM.YY.-.####";
+    
+    // Derived Documents
+    ticket_docname?: string;
+    inanly_docname?: string;
+    child_tracker_table?: ChildTrackerTableDoc[];
+    
+    // Attachments
+    main_eqp_purchase_invoice?: string;
+    main_eqp_serial_no_label_picture?: string;
+    oss_data_extraction?: string;
+    
+    // Failure Information
+    ext_fault_date?: string;
+    ext_fault_code?: string;
+    ext_fault_description?: string;
+    ext_fault_customer_description?: string;
+    
+    // Preliminary Analysis (Check fields)
+    main_eqp_is_online?: number;
+    main_eqp_has_updated?: number;
+    main_eqp_has_reseted?: number;
+    main_eqp_has_neutral?: number;
+    main_eqp_has_stringbox?: number;
+    main_eqp_has_battery?: number;
+    main_eqp_has_transformer?: number;
+    main_eqp_has_sm?: number;
+    main_eqp_has_sem?: number;
+    
+    // Analysis Conclusion
+    solution_description?: string;
+    solution_select?: 
+        | "Parameterization"
+        | "Installation"
+        | "Technical Questions"
+        | "Deep Analysis"
+        | "No Action Needed"
+        | "Update Firmware";
+    
+    // Internal Information
+    priority?: "Maximum" | "Reclame Aqui" | "Legal Process";
+    corrections_tracker?: CorrectionsTrackerDoc[];
+    
+    // System Fields
+    email_applicant?: string;
+    subject_email?: string;
 }
