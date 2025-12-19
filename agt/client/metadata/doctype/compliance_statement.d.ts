ca478a1bd8a781517b009ed1a125da589a16a6cb
Marco Souza <gasparmarco92@gmail.com>
2025-12-19 12:37:35 -0300

    Refactor ComplianceStatement interface workflow actions
    Signed-off-by: Marco Souza <gasparmarco92@gmail.com>

--- agt/client/metadata/doctype/compliance_statement.d.ts
+++ agt/client/metadata/doctype/compliance_statement.d.ts
@@ -3,33 +3,17 @@ import type { WorkflowState } from "../utils/workflow_state";
 
 export interface ComplianceStatement {
   workflow_action: {
-    approve: WorkflowAction;
-    request_analysis: WorkflowAction;
-    request_correction: WorkflowAction;
-    finish_review: WorkflowAction;
+    request_revision: WorkflowAction;
+    finish: WorkflowAction;
     reject: WorkflowAction;
-    finish_service: WorkflowAction;
     cancel: WorkflowAction;
-    finish_correction: WorkflowAction;
-    request_documentation: WorkflowAction;
-    request_checklist: WorkflowAction;
-    forward_to_support: WorkflowAction;
-  } // agt.doctype_namespace.compliance_statement.workflow_action
+    approve: WorkflowAction;
+  },
   workflow_state: {
-    customer_finish_filling: WorkflowState;
-    growatt_preliminary_assessment: WorkflowState;
-    customer_fix_info: WorkflowState;
+    holding_action: WorkflowState;
     growatt_review: WorkflowState;
-    customer_checklist_requested: WorkflowState;
-    checklist_finished: WorkflowState;
-    shipping_proposal: WorkflowState;
-    warranty_approved: WorkflowState;
-    rejected: WorkflowState;
     finished: WorkflowState;
+    rejected: WorkflowState;
     cancelled: WorkflowState;
-    approved: WorkflowState;
-    finished_fixed: WorkflowState;
-    finished_missing: WorkflowState;
-    customer_necessary_action: WorkflowState;
-  } // agt.doctype_namespace.compliance_statement.workflow_state
+  }
 }
