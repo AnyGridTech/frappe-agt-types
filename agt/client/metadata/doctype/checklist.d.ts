bb771261cb19e640d608c9ee0948d060a8fe6f02
Marco Souza <gasparmarco92@gmail.com>
2025-12-19 12:40:28 -0300

    Add workflow_action to Checklist interface
    Signed-off-by: Marco Souza <gasparmarco92@gmail.com>

--- agt/client/metadata/doctype/checklist.d.ts
+++ agt/client/metadata/doctype/checklist.d.ts
@@ -1,12 +1,19 @@
+import type { WorkflowAction } from "../utils/workflow_action";
 import type { WorkflowState } from "../utils/workflow_state";
 
 export interface Checklist {
+  workflow_action: {
+    request_revision: WorkflowAction;
+    finish: WorkflowAction;
+    reject: WorkflowAction;
+    cancel: WorkflowAction;
+    approve: WorkflowAction;
+  },
   workflow_state: {
-    pre_analysis: WorkflowState;
-    customer_fix_info: WorkflowState;
+    holding_action: WorkflowState;
     growatt_review: WorkflowState;
     finished: WorkflowState;
     rejected: WorkflowState;
     cancelled: WorkflowState;
-  } // agt.doctype_namespace.checklist.workflow_state
+  }
 }
